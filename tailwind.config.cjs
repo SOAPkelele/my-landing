bbb4ee0fda1cee91d150543f7e6abe007cb69594
soapkelele <buev_anton@mail.ru>
2023-01-01 19:07:38 +0300

    upd

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {},
+  future: {
+    hoverOnlyWhenSupported: true,
   },
-  plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          background: '#040404',
+          'background-purpose': '#12141D',
+          'perk-gradient-light': 'rgba(4, 6, 14, 0.9)',
+          'perk-gradient-dark': '#04060E',
+        },
+      },
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+  },
 }
